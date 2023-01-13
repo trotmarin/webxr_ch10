5a08d2c1f005e1536e329e74ce2a725784ce8498
admin <trotmarin@gmail.com>
2023-01-13 18:00:34 +0900

    ar name card

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 //
 // app.js is the main entry point for your 8th Wall app. Code here will execute after head.html
 // is loaded, and before body.html is loaded.
-import './index.css'
+// import './index.css'
 // import {nextButtonComponent} from './next-button'
 
 // AFRAME.registerComponent('alert-message')
